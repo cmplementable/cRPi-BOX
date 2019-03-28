ae63a6fb5a45cd785b284bf57a2834b7bfb4bffb
Bogdan Pahomov <36045528+PiterPentester@users.noreply.github.com>
2019-03-28 21:48:53 +0300

    Delete scan_wifi.sh

--- scripts/scan_wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while [ 1 = 1 ]
-do
-echo "Scanned at " $(date)
-echo "Scanned at " $(date) >> scan.txt
-	iwlist wlan0 scan | grep -e ESSID -e Addres -e WPA -e WEP -e None >> scan.txt
-	sleep 5
-done
