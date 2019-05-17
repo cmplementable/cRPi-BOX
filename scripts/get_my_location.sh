f1d9b9671440b4ea708f80d70f855a5e364f6507
Piter Pentester <user@localhost.localdomain>
2019-05-17 13:48:42 -0400

    testFeatures: dir tree edit

--- scripts/get_my_location.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Getting my local ip "
-	rm my_location.txt
-	ifconfig wlan0 | grep -w "inet" >> my_location.txt
-	iwconfig wlan0 | grep -e ESSID >> my_location.txt
-	python3 parse_info.py
